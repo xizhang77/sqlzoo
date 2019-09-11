dc1c6e099cf050209f98596788171ca7b0fad415
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-09-10 18:17:14 -0700

    Update sum_and_count.sql

--- sum_and_count.sql
+++ sum_and_count.sql
@@ -38,4 +38,26 @@ select sum( population ) from world
 	where name in ('Estonia', 'Latvia', 'Lithuania')
 
 
--- 
+-- 6.
+-- For each continent show the continent and number of countries.
+
+select continent, count(name) from world
+	group by continent
+
+
+-- 7.
+-- For each continent show the continent and number of countries with populations of at least 10 million.
+
+select continent, count(name) from world
+	where population >= 10000000
+	group by continent
+
+
+-- 8.
+-- List the continents that have a total population of at least 100 million.
+
+select continent from world
+	group by continent
+	having sum(population) > 100000000
+
+
